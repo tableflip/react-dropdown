caeb64cfe3aff7a3d0adb9ed3555e7b10bba305f
Diogo Silva <fsdiogo@gmail.com>
2019-01-11 11:15:43 +0000

    chore: add transforms example

--- example/example.js
+++ example/example.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Dropdown, DropdownMenu} from '../src'
 
-function AddFileDropdown ({label, background, boxShadow, open, onActivate, onDismiss, alignRight}) {
+function AddFileDropdown ({ label, background, boxShadow, open, onActivate, onDismiss, alignRight, translateX, translateY }) {
   return (
     <Dropdown className='mh3'>
       <button onClick={onActivate} className='br2 bg-blue white pa3 avenir fw5' style={{width: 200}}>
         {label}
       </button>
-      <DropdownMenu open={open} onDismiss={onDismiss} width={200} alignRight={alignRight} background={background} boxShadow={boxShadow}>
+      <DropdownMenu open={open} onDismiss={onDismiss} width={200} alignRight={alignRight} background={background} boxShadow={boxShadow} translateX={translateX} translateY={translateY}>
         <nav className='pv1'>
           <Option href='#'>Add File</Option>
           <Option href='#'>Add Folder</Option>
@@ -41,6 +41,7 @@ class ExamplePage extends React.Component {
         <AddFileDropdown label='default' open={open} onActivate={this.toggleDropdown} onDismiss={this.toggleDropdown} />
         <AddFileDropdown label='aqua background' background='#69c4cd' open={open} onActivate={this.toggleDropdown} onDismiss={this.toggleDropdown} />
         <AddFileDropdown label='pink shadow' boxShadow='0px 1px 10px 0px pink' open={open} onActivate={this.toggleDropdown} onDismiss={this.toggleDropdown} />
+        <AddFileDropdown label='with transforms' translateX={90} translateY={10} open={open} onActivate={this.toggleDropdown} onDismiss={this.toggleDropdown} />
       </div>
     )
   }
